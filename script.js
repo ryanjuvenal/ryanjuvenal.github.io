dc5b3ffd1e282c1cd18f235e23d3aa25013e624c
Ryan Oliveira <140063105+ryanjuvenal@users.noreply.github.com>
2025-06-10 16:03:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,25 +16,25 @@ submit.addEventListener("click", function() {
     var resultado = texto.toLowerCase();
     console.log(resultado)
     if (resultado == "993") {
-        window.location.href = "2.html"
+        window.location.href = "/2.html"
     }
 
     if (resultado == "nigger") {
-        window.location.href = "3.html"
+        window.location.href = "/3.html"
     }
 
     if (resultado == "pixa in" || resultado == "pixa-in") {
-        window.location.href = "4.html"
+        window.location.href = "/4.html"
     } else if(resultado == "pixain"){
         window.alert("Não é assim que se escreve :P")
     }
     if (resultado == "#6b1812" || resultado == "adm") {
-        window.location.href = "5.html"
+        window.location.href = "/5.html"
     } else if(resultado == "vermelho"){
         window.alert("Não é tão fácil assim cara")
     }
     if (resultado == "squirtle") {
-        window.location.href = "6.html"
+        window.location.href = "/6.html"
     } else if(resultado == "squirt"){
         window.alert("Essa não é a nossa forma de falar >:C")
     }
